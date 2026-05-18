ba0bd7c837ebc99be5371921ed21fb17208944b5
nedakhodabakhshi <khodabakhshi.neda@gmail.com>
2026-05-18 17:51:32 +0200

    Working AKS Store Demo with Ingress and ACR fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,9 +48,3 @@ resource "azurerm_role_assignment" "aks_acr_pull" {
   role_definition_name = "AcrPull"
   scope                = azurerm_container_registry.main.id
 }
-
-resource "azurerm_role_assignment" "aks_acr_pull" {
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name = "AcrPull"
-  scope                = azurerm_container_registry.acr.id
-}
